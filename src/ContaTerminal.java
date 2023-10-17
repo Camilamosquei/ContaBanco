2cda15d49f6f04d3975f85ae3f38f2e4834073f7
camila <milladdlovato@gmail.com>
2023-10-16 22:29:57 -0300

    ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,32 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
+ 
+public class    ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+
+        Scanner scanner = new Scanner (System.in).useLocale (Locale.US);
+
+
+
+System.out.println("Bem-vindo ao Banco! Por favor, digite o número da agencia:");
+String Agencia = scanner.next();
+System.out.print ("Agora digite o numero da sua conta:");
+        int NumeroConta = scanner.nextInt ();
+        System.out.print ("nome:");
+ String NomeCliente = scanner.next();
+ System.out.print("Sobrenome:");
+ String Sobrenome = scanner.next();
+                double Saldo = 237.48;
+ System.out.print ("Olá," + NomeCliente +  " " + Sobrenome + " obrigado por criar uma conta em nosso banco de dados, sua agencia é, " + Agencia +" de conta, " + NumeroConta + " e seu saldo " + Saldo + "já está disponivel");
+ 
+
+
+
+
+
+
+
+
     }
 }
